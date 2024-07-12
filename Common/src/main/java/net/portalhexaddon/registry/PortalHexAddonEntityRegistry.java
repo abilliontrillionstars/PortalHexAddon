fc0dc3e6649afcc8e7755ededb8dc8ce2952ca58
Stickia <stick_man@mindlesstoys.com>
2024-07-11 21:21:42 -0400

    Fixed MovePortalOutput still in the book Entities half way done (woo ho, half way there)

--- Common/src/main/java/net/portalhexaddon/registry/PortalHexAddonEntityRegistry.java
+++ Common/src/main/java/net/portalhexaddon/registry/PortalHexAddonEntityRegistry.java
@@ -1,17 +1,45 @@
 package net.portalhexaddon.registry;
 
-import at.petrak.hexcasting.common.items.ItemStaff;
-import dev.architectury.registry.CreativeTabRegistry;
-import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.RegistrySupplier;
-import net.minecraft.core.Registry;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.portalhexaddon.PortalHexAddon;
 
-import static net.portalhexaddon.PortalHexAddon.id;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+
+import net.portalhexaddon.entites.HexPortalEntity;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import static at.petrak.hexcasting.api.HexAPI.modLoc;
+
 
 public class PortalHexAddonEntityRegistry {
 
+
+    public static final EntityType<HexPortalEntity> HEXPORTALENTIY = register(
+            "hexportalentity",
+            EntityType.Builder.of((EntityType.EntityFactory<HexPortalEntity>) HexPortalEntity::new, MobCategory.MISC)
+
+    );
+
+
+
+    public static void registerEntities (BiConsumer<EntityType<?>, ResourceLocation> r) {
+        for (var e : ENTITIES.entrySet()) {
+            r.accept(e.getValue(), e.getKey());
+        }
+    }
+
+    private static final Map<ResourceLocation, EntityType<?>> ENTITIES = new LinkedHashMap<>();
+
+    private static <T extends Entity> EntityType<T> register (String id, EntityType<T> type) {
+        var old = ENTITIES.put(modLoc(id), type);
+        if (old != null) {
+            throw new IllegalArgumentException("Typo? Duplicate id " + id);
+        }
+        return type;
+
+    }
 }
